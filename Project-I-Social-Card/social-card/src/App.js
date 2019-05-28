ef5746265f69e9cc9301f8a8db7a60ccf0a6d58a
hsrsbd <hsraffety@gmail.com>
2019-05-27 18:32:14 -0700

    stretch 1 attempt.

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment } from '@fortawesome/free-solid-svg-icons';
 import './App.scss';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
+
+library.add(faComment);
 
 const App = () => {
   return (
     <div className='App'>
       <HeaderContainer />
       <CardContainer />
+      <Footer />
     </div>
   );
 };
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <FontAwesomeIcon icon='comment' />
+      <FontAwesomeIcon icon='comment' />
+      <FontAwesomeIcon icon='comment' />
+      <FontAwesomeIcon icon='comment' />
+    </div>
+  );
+};
+
+export default Footer;
